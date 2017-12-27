204dba96d502b1033ec56b28debf3a6da91f6303
quanghung97 <hungcan1997@gmail.com>
2017-12-28 04:48:31 +0700

    trash

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.listen(3000);
-<<<<<<< HEAD
-//test 
-=======
-
->>>>>>> 977878efe6edfbfe906b79847edb67b8d7891e36
+//trash
 app.get("/", function(req, res){
    res.render("trangchu");
 });
